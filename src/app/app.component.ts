12ece31e0c69260a87b66cdfbb98d8c25b14cc8e
alejodevelop <65092704+alejocabarcas@users.noreply.github.com>
2023-03-28 14:57:45 -0500

    template-pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exampleUscoID';
+  isAnyViewUpdate?: boolean;
+
+  constructor(private update: SwUpdate) {
+    this.update.checkForUpdate()
+      .then((res) => this.isAnyViewUpdate = res)
+      .catch(err => console.log('there was an error checking for update:' + err))
+  }
+
 }
--- src/app/routes/app-routing.module.ts
+++ src/app/routes/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IdViewComponent } from '../components/id-view/id-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '**', component: IdViewComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
